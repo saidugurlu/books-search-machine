0fc2cdec4a09b2bf388d0c4553b1636f570bb9f0
saidugurlu <ugurlumsaid@gmx.de>
2022-04-07 15:16:23 +0200

    edit

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const urlEn = "https://gutendex.com/books?languages=en";
 const urlDe = "https://gutendex.com/books?languages=de";
 const urlFr = "https://gutendex.com/books?languages=fr";
 const appElem = document.querySelector("#app");
+const filter = document.querySelector("#filter");
+
+let books = [];
+
 appElem.innerHTML = `<div class='tetrominos'>
 <div class='tetromino box1'></div>
 <div class='tetromino box2'></div>
@@ -10,7 +14,6 @@ appElem.innerHTML = `<div class='tetrominos'>
 <div class='tetromino box4'></div>
 </div>`;
 appElem.innerHTML;
-let books = [];
 
 setTimeout(() => {
   (async () => {
@@ -23,20 +26,17 @@ setTimeout(() => {
     const booksEn = await responseEn.json();
     const booksFr = await responseFr.json();
 
-    // console.log(booksDe);
-    // console.log(booksEn);
     books = [...booksDe.results, ...booksEn.results, ...booksFr.results];
-    //console.log("allBooks", books);
+
     appElem.innerHTML = ` <div class="container">
   <ul>
       ${books
         .map((book) => {
-          // console.log(book.authors);
           if (book.authors.length !== 0) {
             return `<li class="bookList">
             <span><a target="_blank" href="${book.formats["application/pdf"]}"><img style="width: 10rem; height: 15rem" src="${book.formats["image/jpeg"]}" alt="Book's image"></a></span>
             <span  class="items">${book.title}</span>
-            <span  ><strong> ${book.authors[0]["name"]}</strong></span>
+            <span class="items" ><strong> ${book.authors[0]["name"]}</strong></span>
             </li>`;
           }
         })
@@ -49,13 +49,11 @@ setTimeout(() => {
 }, 1000);
 
 // Filter items-----------------------------------------------------------------------
-const filter = document.querySelector("#filter");
 
 filter.addEventListener("keyup", filterBooks);
 function filterBooks(e) {
   const filterValue = e.target.value.toLowerCase();
   const listItems = document.querySelectorAll(".items");
-  console.log("said", listItems);
 
   listItems.forEach(function (listItem) {
     const text = listItem.textContent.toLowerCase();
@@ -69,20 +67,3 @@ function filterBooks(e) {
   });
   e.preventDefault();
 }
-// Suchen nach ein Buch
-// filtern wir das Buch aus Books aus der
-// wenn es zutritt dann drücken wir es in appElem.innerHTML aus
-// const searchBook = document.querySelector("#searchBook");
-// searchBook.addEventListener("click", () => {
-//   return books.map((book) => {
-//     // console.log(book.authors);
-//     // console.log(filter.value);
-//     let author = book.authors.length > 0 ? book.authors[0].name : "no author";
-//     if (book.title.includes(filter.value) || author.includes(filter.value)) {
-//       // console.log(book.name);
-//       return book;
-//     } else {
-//       console.log("no name");
-//     }
-//   });
-// });
