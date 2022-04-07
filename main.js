88f56d55074595d25339cda2483a237e4ded2005
saidugurlu <ugurlumsaid@gmx.de>
2022-04-07 08:39:45 +0200

    filter added

--- main.js
+++ main.js
@@ -2,36 +2,36 @@ import "./style.scss";
 const urlEn = "https://gutendex.com/books?languages=en";
 const urlDe = "https://gutendex.com/books?languages=de";
 const urlFr = "https://gutendex.com/books?languages=fr";
-
 const appElem = document.querySelector("#app");
-const filter = document.querySelector("#filter");
-const searchBook = document.querySelector("#searchBook");
-
-appElem.innerHTML;
-let books = [];
-
 appElem.innerHTML = `<div class='tetrominos'>
 <div class='tetromino box1'></div>
 <div class='tetromino box2'></div>
 <div class='tetromino box3'></div>
 <div class='tetromino box4'></div>
 </div>`;
+appElem.innerHTML;
+let books = [];
 
-// fetch
-(async () => {
-  const responseDe = await fetch(urlDe);
-  const responseEn = await fetch(urlEn);
-  const responseFr = await fetch(urlFr);
+setTimeout(() => {
+  (async () => {
+    // fetch
+    const responseDe = await fetch(urlDe);
+    const responseEn = await fetch(urlEn);
+    const responseFr = await fetch(urlFr);
 
-  const booksDe = await responseDe.json();
-  const booksEn = await responseEn.json();
-  const booksFr = await responseFr.json();
-  books = [...booksDe.results, ...booksEn.results, ...booksFr.results];
+    const booksDe = await responseDe.json();
+    const booksEn = await responseEn.json();
+    const booksFr = await responseFr.json();
 
-  appElem.innerHTML = `<div class="container">
+    // console.log(booksDe);
+    // console.log(booksEn);
+    books = [...booksDe.results, ...booksEn.results, ...booksFr.results];
+    console.log("allBooks", books);
+    appElem.innerHTML = ` <div class="container">
   <ul>
       ${books
         .map((book) => {
+          // console.log(book.authors);
           if (book.authors.length !== 0) {
             return `<li class="bookList">
             <span><a target="_blank" href="${book.formats["application/pdf"]}"><img style="width: 10rem; height: 15rem" src="${book.formats["image/jpeg"]}" alt="Book's image"></a></span>
@@ -45,30 +45,34 @@ appElem.innerHTML = `<div class='tetrominos'>
   </ul>
   </div>
 `;
-})();
+  })();
+}, 1000);
 
 // Filter items-----------------------------------------------------------------------
-// filter.addEventListener("keyup", filterBooks);
+const filter = document.querySelector("#filter");
 
-// function filterBooks(e) {
-//   const filterValue = e.target.value.toLowerCase();
-//   const listItems = document.querySelectorAll(".items");
-//   listItems.forEach(function (listItem) {
-//     const text = listItem.textContent.toLowerCase();
-//     if (text.indexOf(filterValue) === -1) {
-//       //not find
-//       listItem.parentElement.setAttribute("style", "display:none !important");
-//     } else {
-//       listItem.parentElement.setAttribute("style", "display:flex");
-//     }
-//   });
-//   e.preventDefault();
-// }
- 
+filter.addEventListener("keyup", filterBooks);
+function filterBooks(e) {
+  const filterValue = e.target.value.toLowerCase();
+  const listItems = document.querySelectorAll(".items");
 
+  // console.log("said", listItems);
+  listItems.forEach(function (listItem) {
+    const text = listItem.textContent.toLowerCase();
+    // console.log(text);
+    if (text.indexOf(filterValue) === -1) {
+      //not find
+      listItem.parentElement.setAttribute("style", "display:none !important");
+    } else {
+      listItem.parentElement.setAttribute("style", "display:block");
+    }
+  });
+  e.preventDefault();
+}
 // Suchen nach ein Buch
 // filtern wir das Buch aus Books aus der
 // wenn es zutritt dann drücken wir es in appElem.innerHTML aus
+const searchBook = document.querySelector("#searchBook");
 searchBook.addEventListener("click", () => {
   return books.map((book) => {
     // console.log(book.authors);
